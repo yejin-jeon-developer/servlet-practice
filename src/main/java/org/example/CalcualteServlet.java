1a127e69bf65140fe92cea62b19ca2e792f7951c
jeonye <yemong1226@gmail.com>
2023-03-28 23:01:53 +0900

    HttpServlet 실습코드 추가

--- src/main/java/org/example/CalcualteServlet.java
+++ src/main/java/org/example/CalcualteServlet.java
@@ -1,38 +1,32 @@
 package org.example;
 
+import org.example.calculate.Calculator;
+import org.example.calculate.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/calculate")
-public class CalcualteServlet implements Servlet{
+public class CalcualteServlet extends HttpServlet {
     private static final Logger logger = LoggerFactory.getLogger(CalcualteServlet.class);
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        logger.info("init");
-    }
 
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         logger.info("service");
+        int operand1 = Integer.parseInt(request.getParameter("operand1"));
+        String operator = request.getParameter("operator");
+        int operand2 = Integer.parseInt(request.getParameter("operand2"));
+
+        int result = Calculator.calcuate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println(result);
     }
-
-    @Override
-    public void destroy() {
-
-    }
-
-    @Override
-    public ServletConfig getServletConfig() {
-        return null;
-    }
-
-    @Override
-    public String getServletInfo() {
-        return null;
-    }
-
 }
